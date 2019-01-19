37372efd08c1d18d247e6974c1b278c4bb94537b
Chris Penner <christopher.penner@gmail.com>
2019-01-19 15:19:32 -0600

    Initial transpose

--- src/Data/Grid/Internal/Transpose.hs
+++ src/Data/Grid/Internal/Transpose.hs
@@ -1,19 +1,40 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE TypeApplications #-}
 module Data.Grid.Internal.Transpose where
 
 import Data.Grid.Internal.Grid
+import Data.Grid.Internal.Coord
 import GHC.TypeNats
 import Data.Singletons.Prelude
 import Data.Singletons.Prelude.List
 import Data.Singletons.Prelude.Maybe
+import Data.Functor.Rep
+import Data.Vector as V
 
-class Transposable from to xs ys where
-  transpose :: Grid xs a -> Grid ys a
+-- class Transposable (key :: [Nat]) from ys where
+--   transpose :: Grid from a -> Grid ys a
 
-instance Transposable '[n] '[n] '[x] '[x] where
-  transpose = id
+-- type family Transposed key from where
+--   Transposed _ _ = [2, 2]
 
-type family GetNextDim (from :: [Nat]) (to :: [Nat]) (xs :: [Nat]) = (next  :: [Nat]) where
-  GetNextDim '[] _ _ = '[]
-  GetNextDim (f:fs) ts xs = (xs !! FromJust (ElemIndex f ts)) : GetNextDim fs ts xs
+transpose :: forall (key :: [Nat]) from to a. (SingI key, Indexable from Clamp, Indexable to Clamp) => Grid from a -> Grid to a
+transpose g@(Grid v) = Grid newVector'
+  where
+    transpMap :: Grid from (Coord to Clamp)
+    transpMap = tabulate (transposeCoord @key)
+    vTranspMap :: V.Vector (Coord to Clamp)
+    vTranspMap = toVector transpMap
+    newVector :: Vector Int
+    newVector = fmap fromEnum vTranspMap
+    newVector' :: Vector a
+    newVector' = V.generate (V.length v) (\n -> v V.! (newVector V.! n))
+
+transposeCoord :: forall (key :: [Nat]) from ys ind. SingI key => Coord from ind -> Coord ys ind
+transposeCoord (Coord cs) = Coord newCoord
+ where
+  key :: [Int]
+  key = fromIntegral <$> demote @key
+  newCoord :: [Int]
+  newCoord = (key !!) <$> cs
