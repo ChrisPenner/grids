f391a27edd93ddd3d94852a4ef1fde5791e2d4f3
Chris Penner <christopher.penner@gmail.com>
2019-03-10 15:42:44 +0100

    Add benchmarks for gaussian blur

--- benchmarks/Benchmarks.hs
+++ benchmarks/Benchmarks.hs
@@ -1,15 +1,37 @@
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 module Benchmarks where
 
 import Gauge.Main
 import Data.Grid as G
+import Data.Functor.Compose
 
-benchTranspose :: forall dims a x y. (Dimensions dims) =>  (dims ~ [x, y]) => Int -> (Grid dims Int, Grid dims Int, Grid dims Int)
-benchTranspose a = (transpose $ pure 1, transpose $ pure 2, transpose $ pure 3)
+benchGauss :: forall window dims x y.
+           (IsGrid dims, IsGrid window, dims ~ [x, y])
+           => Grid dims Double
+           -> Grid dims Double
+benchGauss = gauss @window
+
+idGrid :: IsGrid dims => Grid dims Int
+idGrid = generate id
+
+gauss :: forall window dims. (IsGrid dims, IsGrid window) => Grid dims Double -> Grid dims Double
+gauss = autoConvolute omitBounds gauss'
+  where
+    gauss' :: Compose (Grid window) Maybe Double -> Double
+    gauss' g = (sum g) / fromIntegral (length g)
+
 
 main :: IO ()
-main = defaultMain
-  [ bgroup
-      "permutations"
-      [ bench "nf [150, 150]" $ nf (benchTranspose @[200, 200]) 2
-      ]
-  ]
+main =
+  defaultMain [ bgroup "convolution"
+                       [ bgroup "gauss"
+                                [ bench "nf [3, 3] window over [100, 100]"
+                                    $ nf (benchGauss @[3, 3]) (fromIntegral <$> idGrid @[100, 100])
+                                , bench "nf [10, 10] window over [100, 100]"
+                                    $ nf (benchGauss @[10, 10]) (fromIntegral <$> idGrid @[100, 100])
+                                , bench "nf [3, 3] window over [500, 500]"
+                                    $ nf (benchGauss @[3, 3]) (fromIntegral <$> idGrid @[500, 500])
+                                ]
+                       ]
+              ]
