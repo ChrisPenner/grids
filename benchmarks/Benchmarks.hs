25bc1e2272f1aeff9a0979200788b97a45b741b4
Chris Penner <christopher.penner@gmail.com>
2019-03-20 00:02:58 +0100

    Add benchmarks for Sobel

--- benchmarks/Benchmarks.hs
+++ benchmarks/Benchmarks.hs
@@ -6,32 +6,69 @@ import Gauge.Main
 import Data.Grid as G
 import Data.Functor.Compose
 
-benchGauss :: forall window dims x y.
+main :: IO ()
+main =
+    defaultMain [ bgroup "convolution"
+                         [ bgroup "boxBlur"
+                                  [ bench "nf [3, 3] window over [100, 100]"
+                                        $ nf (benchBlur @[3, 3])
+                                             (doubleGrid @[100, 100])
+                                  , bench "nf [10, 10] window over [100, 100]"
+                                        $ nf (benchBlur @[10, 10])
+                                             (doubleGrid @[100, 100])
+                                  , bench "nf [3, 3] window over [500, 500]"
+                                        $ nf (benchBlur @[3, 3])
+                                             (doubleGrid @[500, 500])
+                                  ]
+                         , bgroup "sobel"
+                                  [ bench "nf [100, 100]"
+                                        $ nf benchSobel
+                                             (doubleGrid @[100, 100])
+                                  , bench "nf [10, 10] window over [100, 100]"
+                                        $ nf benchSobel
+                                             (doubleGrid @[100, 100])
+                                  , bench "nf [3, 3] window over [500, 500]"
+                                        $ nf benchSobel
+                                             (doubleGrid @[500, 500])
+                                  ]
+                         ]
+                ]
+
+doubleGrid :: IsGrid dims => Grid dims Double
+doubleGrid = fromIntegral <$> generate id
+
+benchBlur :: forall window dims x y.
            (IsGrid dims, IsGrid window, dims ~ [x, y])
            => Grid dims Double
            -> Grid dims Double
-benchGauss = gauss @window
+benchBlur = boxBlur @window
 
-idGrid :: IsGrid dims => Grid dims Int
-idGrid = generate id
 
-gauss :: forall window dims. (IsGrid dims, IsGrid window) => Grid dims Double -> Grid dims Double
-gauss = autoConvolute omitBounds gauss'
+boxBlur :: forall window dims. (IsGrid dims, IsGrid window) => Grid dims Double -> Grid dims Double
+boxBlur = autoConvolute omitBounds gauss'
   where
     gauss' :: Compose (Grid window) Maybe Double -> Double
     gauss' g = (sum g) / fromIntegral (length g)
 
+benchSobel :: IsGrid dims => Grid dims Double -> Grid dims Double
+benchSobel = autoConvolute clampBounds sobel
 
-main :: IO ()
-main =
-  defaultMain [ bgroup "convolution"
-                       [ bgroup "gauss"
-                                [ bench "nf [3, 3] window over [100, 100]"
-                                    $ nf (benchGauss @[3, 3]) (fromIntegral <$> idGrid @[100, 100])
-                                , bench "nf [10, 10] window over [100, 100]"
-                                    $ nf (benchGauss @[10, 10]) (fromIntegral <$> idGrid @[100, 100])
-                                , bench "nf [3, 3] window over [500, 500]"
-                                    $ nf (benchGauss @[3, 3]) (fromIntegral <$> idGrid @[500, 500])
-                                ]
-                       ]
-              ]
+sobel :: Floating a => Grid [3, 3] a -> a
+sobel g = sqrt $ (v ** 2) + (h ** 2)
+    where
+        v = sum (g * sobelVertical)
+        h = sum (g * sobelHorizontal)
+
+sobelHorizontal :: Floating a => Grid [3, 3] a
+sobelHorizontal = fromNestedLists'
+    [ [-1, -2, -1]
+    , [0, 0, 0]
+    , [1, 2, 1]
+    ]
+
+sobelVertical :: Floating a => Grid [3, 3] a
+sobelVertical = fromNestedLists'
+    [ [-1, 0, 1]
+    , [-2, 0, 2]
+    , [-1, 0, 1]
+    ]
